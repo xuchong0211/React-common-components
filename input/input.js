3c30033ea663203bd153a9583366e9824cba1c73
xu.chogn <xu.chong@tooieapp.com>
2016-10-20 10:47:42 +0800

    add a input react compoment

---
+++ input/input.js
@@ -0,0 +1,32 @@
+/**
+ * Created by xuchong on 10/18/16.
+ */
+import React, { PropTypes } from 'react';
+import cn from 'classnames';
+
+const getPlacehold = (field) => {
+    switch (field) {
+        case 'name' :
+            return '请填写您的姓名';
+        case 'phone' :
+            return '请填写你的联系电话';
+        default :   
+            return '';
+    }
+};
+
+const InputField = ({field, value, onChange}) =>
+    <div className={cn(style.title, style[field])}/>
+        <input type="text" value={value} placeholder={getPlacehold(field)} onChange={
+                (event) => {
+                    let args = {};
+                    args[field] = event.target.value;
+                    onChange(args);
+                }
+            }
+        />
+    </div>
+  
+};
+
+export InputField;
